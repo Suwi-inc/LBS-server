cb332b2cb39b338e8c6073f7ef1645d95e7cb98d
Suwi-inc <suwilanjisilwamba@gmail.com>
2024-05-04 20:34:22 +0300

    fix env config on flask app

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,13 +1,17 @@
-# from os import getenv
-# from dotenv import load_dotenv
+from os import getenv
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 db = SQLAlchemy()
 
+app_config = {
+    "SQLALCHEMY_DATABASE_URI": getenv("DB_URI")
+}
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.config["SQLALCHEMY_DATABASE_URI"] = app_config.get("SQLALCHEMY_DATABASE_URI")
     db.init_app(app)
     return app
