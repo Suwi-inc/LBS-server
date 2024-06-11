dcad39879f420578665b6e7fe1cf4aaa8c839ec8
Suwi-inc <suwilanjisilwamba@gmail.com>
2024-06-11 11:52:01 +0300

    update wifi endpoint

--- LBS/main/controller/wifi_network.py
+++ LBS/main/controller/wifi_network.py
@@ -2,15 +2,12 @@ from flask import Blueprint, jsonify, request
 
 from ..service.wifi_service import get_wifinetworks_service
 from ..utils.data_validator import validate_device_data
+from ..auth.auth_guard import auth_guard
 
 wifi = Blueprint("wifi", __name__)
 
 
 @wifi.route("/", methods=["GET"])
+@auth_guard()
 def get_wifinetworks():
-    data = request.get_json()
-
-    if not validate_device_data(data):
-        return jsonify({"Fail": "Missing device data"}), 422
-
     return get_wifinetworks_service()
